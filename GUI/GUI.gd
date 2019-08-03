d1b7046eebe9915353df0a93c6d02f5f511b0f26
Sean Morris <sean@oblate.me>
2019-08-03 17:08:14 -0400

    Added coins, npc's, and an end game portal with particle effects.

--- GUI/GUI.gd
+++ GUI/GUI.gd
@@ -3,6 +3,7 @@ extends CanvasLayer
 func _ready():
 	$Control/TextureRect/HBoxContainer/LifeCount.text = "3"
 	
-func update_lives(lives_left):
+func update_GUI(lives_left, coins):
 	$Control/TextureRect/HBoxContainer/LifeCount.text = String(lives_left)
-		
+	$Control/TextureRect/HBoxContainer/CoinCount.text = String(coins)
+	
